f6f93de14f30972c0d29fdda4c98b4334792743d
kale-io <cos+a@iuvo.ai>
2019-01-29 17:54:36 +0000

    Convert Non-strict to strict equality checking Convert non-strict equality checking, using `==`, to the strict version, using `===`.

--- app/assets/v2/js/pages/kudos_bulk_receive.js
+++ app/assets/v2/js/pages/kudos_bulk_receive.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  if (typeof web3 == 'undefined') {
+  if (typeof web3 === 'undefined') {
     _alert({ message: gettext('You are not on a web3 browser.  Please switch to a web3 browser.') }, 'error');
     $('#receive').attr('disabled', 'disabled');
   }
@@ -8,9 +8,9 @@ $(document).ready(function() {
   }
 
   waitforWeb3(function() {
-    if (document.web3network == 'locked') {
+    if (document.web3network === 'locked') {
       _alert('Metamask not connected. <button id="metamask_connect" onclick="approve_metamask()">Click here to connect to metamask</button>', 'error');
-    } else if (document.web3network != document.network) {
+    } else if (document.web3network !== document.network) {
       _alert({ message: gettext('You are not on the right web3 network.  Please switch to ') + document.network }, 'error');
       $('#receive').attr('disabled', 'disabled');
     } else {
